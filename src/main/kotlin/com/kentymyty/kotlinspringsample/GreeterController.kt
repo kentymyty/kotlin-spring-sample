7cfab5844b0c7f43afe101f341cf4a72de4d552b
Kento Yoneda <boc567ky@gmail.com>
2024-02-16 09:10:52 +0900

    POSTのJSONパラメータを取得する。

--- src/main/kotlin/com/kentymyty/kotlinspringsample/GreeterController.kt
+++ src/main/kotlin/com/kentymyty/kotlinspringsample/GreeterController.kt
@@ -1,8 +1,11 @@
 package com.kentymyty.kotlinspringsample
 
+import com.kentymyty.kotlinspringsample.request.HelloRequest
 import com.kentymyty.kotlinspringsample.response.HelloResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -19,5 +22,10 @@ class GreeterController {
     @GetMapping("/hello/{name}")
     fun helloPathValue(@PathVariable("name") name: String): HelloResponse {
         return HelloResponse("Hello ${name}")
+    }
+
+    @PostMapping("/hello")
+    fun helloByPost(@RequestBody request: HelloRequest): HelloResponse {
+        return HelloResponse("Hello ${request.name}")
     }
 }
