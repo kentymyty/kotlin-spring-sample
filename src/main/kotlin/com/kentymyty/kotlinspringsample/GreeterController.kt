e6d25747ce32b91829e382fbe2265939591a2b41
Kento Yoneda <boc567ky@gmail.com>
2024-02-19 20:28:06 +0900

    DIのサンプル。

--- src/main/kotlin/com/kentymyty/kotlinspringsample/GreeterController.kt
+++ src/main/kotlin/com/kentymyty/kotlinspringsample/GreeterController.kt
@@ -1,7 +1,11 @@
 package com.kentymyty.kotlinspringsample
 
+import com.kentymyty.kotlinspringsample.Di.Greeter
+import com.kentymyty.kotlinspringsample.Di.MessageService
 import com.kentymyty.kotlinspringsample.request.HelloRequest
 import com.kentymyty.kotlinspringsample.response.HelloResponse
+import org.apache.logging.log4j.message.Message
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -12,7 +16,12 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("greeter")
-class GreeterController {
+class GreeterController(
+    private val greeter: Greeter,
+    @Qualifier("EnglishMessageService")
+    private val messageService: MessageService
+)
+{
 
     @GetMapping("/hello")
     fun hello(@RequestParam("name") name: String): HelloResponse {
@@ -27,5 +36,17 @@ class GreeterController {
     @PostMapping("/hello")
     fun helloByPost(@RequestBody request: HelloRequest): HelloResponse {
         return HelloResponse("Hello ${request.name}")
+    }
+
+    @GetMapping("/hello/byservice/{name}")
+    fun helloByService(@PathVariable("name") name: String): HelloResponse {
+        val message = greeter.sayHello(name)
+        return HelloResponse(message)
+    }
+
+    @GetMapping("/hello/qualifier")
+    fun helloByQualifer(): HelloResponse {
+        val message = messageService.sayHello()
+        return HelloResponse(message)
     }
 }
--- src/main/kotlin/com/kentymyty/kotlinspringsample/response/HelloResponse.kt
+++ src/main/kotlin/com/kentymyty/kotlinspringsample/response/HelloResponse.kt
@@ -1,3 +1,3 @@
 package com.kentymyty.kotlinspringsample.response
 
-data class HelloResponse (val massege: String)
+data class HelloResponse (val messege: String)
